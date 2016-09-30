5f8372a12f56e4b7daa38007826c802c7e317b26
Francesco Ballarin <francesco.ballarin@sissa.it>
2016-09-30 08:57:54 +0200

    Clarify license in RBniCS interface

--- block_ext/RBniCS/backends/block_ext/transpose.py
+++ block_ext/RBniCS/backends/block_ext/transpose.py
@@ -1,19 +1,20 @@
+# Copyright (C) 2015-2016 by the RBniCS authors
 # Copyright (C) 2016 by the block_ext authors
 #
-# This file is part of block_ext.
+# This file is part of the RBniCS interface to block_ext.
 #
-# block_ext is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
+# RBniCS and block_ext are free software: you can redistribute them and/or modify
+# them under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# block_ext is distributed in the hope that it will be useful,
+# RBniCS and block_ext are distributed in the hope that they will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with block_ext. If not, see <http://www.gnu.org/licenses/>.
+# along with RBniCS and block_ext. If not, see <http://www.gnu.org/licenses/>.
 #
 
 from RBniCS.backends.basic import transpose as basic_transpose
