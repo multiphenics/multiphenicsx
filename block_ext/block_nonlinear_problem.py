1639ad243bd504308d2ff40b8b1553f44798281a
Francesco Ballarin <francesco.ballarin@sissa.it>
2016-12-10 14:38:55 +0100

    Improve block nonlinear problem to handle assembled block tensors

--- block_ext/block_nonlinear_problem.py
+++ block_ext/block_nonlinear_problem.py
@@ -16,24 +16,28 @@
 # along with block_ext. If not, see <http://www.gnu.org/licenses/>.
 #
 
+import types
+from numpy import ndarray as array
 from dolfin import NonlinearProblem, as_backend_type
 from block_ext.block_assemble import block_assemble
+from block_ext.block_matrix import BlockMatrix
+from block_ext.block_vector import BlockVector
 from block_ext.monolithic_matrix import MonolithicMatrix
 from block_ext.monolithic_vector import MonolithicVector
 
 class BlockNonlinearProblem(NonlinearProblem):
-    def __init__(self, residual_form, block_solution, bcs, jacobian_form):
+    def __init__(self, residual_form_or_eval, block_solution, bcs, jacobian_form_or_eval):
         NonlinearProblem.__init__(self)
         # Store the input arguments
-        self.residual_form = residual_form
-        self.jacobian_form = jacobian_form
+        self.residual_form_or_eval = residual_form_or_eval
+        self.jacobian_form_or_eval = jacobian_form_or_eval
         self.block_solution = block_solution
         self.bcs = bcs
         # Assemble residual and jacobian in order to
         # have block storage with appropriate sizes
         # and sparsity patterns
-        self.block_residual = block_assemble(residual_form)
-        self.block_jacobian = block_assemble(jacobian_form)
+        self.block_residual = self._block_residual_vector_assemble(None, block_solution)
+        self.block_jacobian = self._block_jacobian_matrix_assemble(None, block_solution)
         # Add monolithic wrappers to PETSc objects, initialized
         # the first time F or J are called
         self.monolithic_residual = None
@@ -52,7 +56,6 @@ class BlockNonlinearProblem(NonlinearProblem):
         if self.monolithic_residual is None:
             self.monolithic_residual = MonolithicVector(self.block_residual, as_backend_type(fenics_residual).vec(), block_discard_dofs=self.block_discard_dofs)
         # Shorthands
-        residual_form = self.residual_form
         block_residual = self.block_residual
         block_solution = self.block_solution
         monolithic_residual = self.monolithic_residual # wrapper of the second input argument
@@ -61,29 +64,76 @@ class BlockNonlinearProblem(NonlinearProblem):
         # Convert monolithic_solution into block_solution
         monolithic_solution.copy_values_to(block_solution.block_vector())
         # Assemble
-        block_assemble(residual_form, block_tensor=block_residual)
+        self._block_residual_vector_assemble(block_residual, block_solution)
         bcs.apply(block_residual, block_solution.block_vector())
         # Copy values from block_residual/solution into monolithic_residual/solution
         monolithic_residual.zero(); monolithic_residual.block_add(block_residual)
         monolithic_solution.zero(); monolithic_solution.block_add(block_solution.block_vector())
         
-        from petsc4py import PETSc
-        b_viewer = PETSc.Viewer().createASCII("r.m", comm= PETSc.COMM_WORLD)
-        b_viewer.pushFormat(PETSc.Viewer.Format.ASCII_MATLAB)
-        b_viewer.view(monolithic_residual.vec())
-        b_viewer.popFormat()
+    def _block_residual_vector_assemble(self, block_residual, block_solution):
+        assert isinstance(self.residual_form_or_eval, (list, array, types.FunctionType))
+        if isinstance(self.residual_form_or_eval, (list, array)):
+            residual_form_or_vector = self.residual_form_or_eval
+        elif isinstance(self.residual_form_or_eval, types.FunctionType):
+            residual_form_or_vector = self.residual_form_or_eval(block_solution)
+        else:
+            raise AssertionError("Invalid case in BlockNonlinearProblem._block_residual_vector_assemble.")
+        assert isinstance(residual_form_or_vector, (list, array, BlockVector))
+        if isinstance(residual_form_or_vector, (list, array)):
+            if block_residual is not None:
+                block_assemble(residual_form_or_vector, block_tensor=block_residual)
+                return block_residual
+            else:
+                return block_assemble(residual_form_or_vector)
+        elif isinstance(residual_form_or_vector, BlockVector):
+            N = len(residual_form_or_vector)
+            if block_residual is None:
+                block_residual = BlockVector(N)
+            for I in range(N):
+                block_residual.blocks[I] = residual_form_or_vector.blocks[I]
+            return block_residual
+        else:
+            raise AssertionError("Invalid case in BlockNonlinearProblem._block_residual_vector_assemble.")
         
     def J(self, fenics_jacobian, _):
         # Initialize monolithic wrappers (only once)
         if self.monolithic_jacobian is None:
             self.monolithic_jacobian = MonolithicMatrix(self.block_jacobian, as_backend_type(fenics_jacobian).mat(), block_discard_dofs=self.block_discard_dofs)
         # Shorthands
-        jacobian_form = self.jacobian_form
         block_jacobian = self.block_jacobian
+        block_solution = self.block_solution
         bcs = self.bcs
         monolithic_jacobian = self.monolithic_jacobian # wrapper of the second input argument
         # Assemble
-        block_assemble(jacobian_form, block_tensor=block_jacobian)
+        self._block_jacobian_matrix_assemble(block_jacobian, block_solution)
         bcs.apply(block_jacobian)
         # Copy values from block_jacobian into monolithic_jacobian
         monolithic_jacobian.zero(); monolithic_jacobian.block_add(block_jacobian)
+        
+    def _block_jacobian_matrix_assemble(self, block_jacobian, block_solution):
+        assert isinstance(self.jacobian_form_or_eval, (list, array, types.FunctionType))
+        if isinstance(self.jacobian_form_or_eval, (list, array)):
+            jacobian_form_or_matrix = self.jacobian_form_or_eval
+        elif isinstance(self.jacobian_form_or_eval, types.FunctionType):
+            jacobian_form_or_matrix = self.jacobian_form_or_eval(block_solution)
+        else:
+            raise AssertionError("Invalid case in BlockNonlinearProblem._block_jacobian_matrix_assemble.")
+        assert isinstance(jacobian_form_or_matrix, (list, array, BlockMatrix))
+        if isinstance(jacobian_form_or_matrix, (list, array)):
+            if block_jacobian is not None:
+                block_assemble(jacobian_form_or_matrix, block_tensor=block_jacobian)
+                return block_jacobian
+            else:
+                return block_assemble(jacobian_form_or_matrix)
+        elif isinstance(jacobian_form_or_matrix, BlockMatrix):
+            N = len(jacobian_form_or_matrix)
+            M = len(jacobian_form_or_matrix[0])
+            if block_jacobian is None:
+                block_jacobian = BlockMatrix(N, M)
+            # Assemble
+            for I in range(N):
+                for J in range(M):
+                    block_jacobian.blocks[I, J] = jacobian_form_or_matrix.blocks[I, J]
+            return block_jacobian
+        else:
+            raise AssertionError("Invalid case in BlockNonlinearProblem._block_jacobian_matrix_assemble.")
