ca9f699754f2520392a377abbf248dffd07f66bb
Francesco Ballarin <francesco.ballarin@sissa.it>
2016-12-11 14:53:00 +0100

    Undo overriding of Generic{Matrix,Vector} arithmetic operators carried out by CBC block

--- block_ext/__init__.py
+++ block_ext/__init__.py
@@ -16,9 +16,39 @@
 # along with block_ext. If not, see <http://www.gnu.org/licenses/>.
 #
 
+# 1. Undo modifications to FEniCS by CBC block
+import sys
+assert "block" not in sys.modules, "block_ext needs to undo modifications to FEniCS matrix/vector carried out by CBC block"
+
+from dolfin import GenericMatrix, Matrix
+fenics_matrix_operators_names = ("__add__", "__radd__", "__iadd__", "__sub__", "__rsub__", "__isub__", "__mul__", "__rmul__", "__imul__", "__div__", "__rdiv__", "__idiv__", "__truediv__", "__itruediv__")
+fenics_matrix_operators_backup = dict()
+for operator in fenics_matrix_operators_names:
+    fenics_matrix_operators_backup[(GenericMatrix, operator)] = getattr(GenericMatrix, operator)
+    fenics_matrix_operators_backup[(Matrix, operator)] = getattr(Matrix, operator)
+
+from dolfin import GenericVector, Vector
+fenics_vector_operators_names = ("__add__", "__radd__", "__iadd__", "__sub__", "__rsub__", "__isub__", "__mul__", "__rmul__", "__imul__", "__div__", "__rdiv__", "__idiv__", "__truediv__", "__itruediv__")
+fenics_vector_operators_backup = dict()
+for operator in fenics_vector_operators_names:
+    fenics_vector_operators_backup[(GenericVector, operator)] = getattr(GenericVector, operator)
+    fenics_vector_operators_backup[(Vector, operator)] = getattr(Vector, operator)
+    
+import block
+
+for operator in fenics_matrix_operators_names:
+    setattr(GenericMatrix, operator, fenics_matrix_operators_backup[(GenericMatrix, operator)])
+    setattr(Matrix, operator, fenics_matrix_operators_backup[(Matrix, operator)])
+    
+for operator in fenics_vector_operators_names:
+    setattr(GenericVector, operator, fenics_vector_operators_backup[(GenericVector, operator)])
+    setattr(Vector, operator, fenics_vector_operators_backup[(Vector, operator)])
+    
+# 2. Import petsc4py
 import sys, petsc4py
 petsc4py.init(sys.argv)
 
+# 3. Import block_ext
 from block_ext.block_assemble import block_assemble
 from block_ext.block_derivative import block_derivative
 from block_ext.block_dirichlet_bc import BlockDirichletBC
