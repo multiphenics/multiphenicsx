210a8308208dfd4c72634d584852b1a8fc85f46c
Francesco Ballarin <francesco.ballarin@sissa.it>
2017-05-31 13:55:06 +0200

    Add new example 1b to tutorial 6, on elliptic optimal control with distributed control. Contributed by Zakia Zainib <zzainib@sissa.it>

---
+++ tutorials/6_optimal_control/1b_poisson.py
@@ -0,0 +1,109 @@
+# Copyright (C) 2016-2017 by the multiphenics authors
+#
+# This file is part of multiphenics.
+#
+# multiphenics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# multiphenics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from dolfin import *
+from multiphenics import *
+
+"""
+In this tutorial we solve the optimal control problem
+
+min J(y, u) = 1/2 \int_{\Omega} (y - y_d)^2 dx + \alpha/2 \int_{\Omega} u^2 dx
+s.t.
+    - \Delta y = f + u   in \Omega
+             y = 1       on \partial \Omega
+             
+where
+    \Omega                      unit square
+    u \in L^2(\Omega)           control variable
+    y \in H^1_0(\Omega)         state variable
+    \alpha > 0                  penalization parameter
+    y_d = piecewise constant    desired state
+    f                           forcing term
+    
+using an adjoint formulation solved by a one shot approach.
+The test case is from section 5.1 of
+F. Negri, G. Rozza, A. Manzoni and A. Quarteroni. Reduced Basis Method for Parametrized Elliptic Optimal Control Problems. SIAM Journal on Scientific Computing, 35(5): A2316-A2340, 2013.
+"""
+
+## MESH ##
+mesh = Mesh("data/rectangle.xml")
+subdomains = MeshFunction("size_t", mesh, "data/rectangle_physical_region.xml")
+boundaries = MeshFunction("size_t", mesh, "data/rectangle_facet_region.xml")
+
+## FUNCTION SPACES ##
+Y = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+U = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+Q = Y
+W_el = BlockElement(Y, U, Q)
+W = BlockFunctionSpace(mesh, W_el)
+
+## PROBLEM DATA ##
+alpha = Constant(0.01)
+y_d_1 = Constant(1.0)
+y_d_2 = Constant(0.6)
+f = Constant(0.)
+
+## TRIAL/TEST FUNCTIONS ##
+yup = BlockTrialFunction(W)
+(y, u, p) = block_split(yup)
+zvq = BlockTestFunction(W)
+(z, v, q) = block_split(zvq)
+
+## MEASURES ##
+dx = Measure("dx")(subdomain_data=subdomains)
+
+## OPTIMALITY CONDITIONS ##
+a = [[y*z*dx                    , 0           , inner(grad(p), grad(z))*dx], 
+     [0                         , alpha*u*v*dx, - p*v*dx                  ],
+     [inner(grad(y), grad(q))*dx, - u*q*dx    , 0                         ]]
+f =  [y_d_1*z*dx(1) + y_d_2*z*dx(2),
+      0                            ,
+      f*q*dx                        ]
+bc = BlockDirichletBC([[DirichletBC(W.sub(0), Constant(1.), boundaries, 1)],
+                       [],
+                       [DirichletBC(W.sub(2), Constant(0.), boundaries, 1)]])
+
+## SOLUTION ##
+yup = BlockFunction(W)
+(y, u, p) = block_split(yup)
+
+## FUNCTIONAL ##
+J = 0.5*inner(y - y_d_1, y - y_d_1)*dx(1) + 0.5*inner(y - y_d_2, y - y_d_2)*dx(2) + 0.5*alpha*inner(u, u)*dx
+
+## UNCONTROLLED FUNCTIONAL VALUE ##
+A_state = assemble(a[2][0])
+F_state = assemble(f[2])
+[bc_state.apply(A_state) for bc_state in bc[0]]
+[bc_state.apply(F_state)  for bc_state in bc[0]]
+solve(A_state, y.vector(), F_state)
+print "Uncontrolled J =", assemble(J)
+plot(y, title="uncontrolled state")
+interactive()
+
+## OPTIMAL CONTROL ##
+A = block_assemble(a)
+F = block_assemble(f)
+bc.apply(A)
+bc.apply(F)
+block_solve(A, yup.block_vector(), F)
+print "Optimal J =", assemble(J)
+plot(y, title="state")
+plot(u, title="control")
+plot(p, title="adjoint")
+interactive()
+
---
+++ tutorials/6_optimal_control/data/generate_mesh_rectangle.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2016-2017 by the multiphenics authors
+#
+# This file is part of multiphenics.
+#
+# multiphenics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# multiphenics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from dolfin import *
+from mshr import *
+from multiphenics import *
+
+"""
+This file generates the mesh which is used in the following examples:
+    1b_poisson
+The test case is from section 5.1 of
+F. Negri, G. Rozza, A. Manzoni and A. Quarteroni. Reduced Basis Method for Parametrized Elliptic Optimal Control Problems. SIAM Journal on Scientific Computing, 35(5): A2316-A2340, 2013.
+"""
+
+# Create mesh
+square = Rectangle(Point(0, 0), Point(1, 1))
+rectangle = Rectangle(Point(1, 0), Point(4, 1))
+domain = square + rectangle
+domain.set_subdomain(1, square)
+domain.set_subdomain(2, rectangle)
+mesh = generate_mesh(domain, 64)
+plot(mesh)
+interactive()
+
+# Create subdomains
+subdomains = MeshFunction("size_t", mesh, mesh.topology().dim(), mesh.domains())
+plot(subdomains)
+interactive()
+
+# Create boundaries
+class Boundary(SubDomain):
+    def inside(self, x, on_boundary):
+        return on_boundary
+        
+boundaries = FacetFunction("size_t", mesh)
+boundary = Boundary()
+boundary.mark(boundaries, 1)
+plot(boundaries)
+interactive()
+
+# Save
+File("rectangle.xml") << mesh
+File("rectangle.pvd") << mesh
+File("rectangle_physical_region.xml") << subdomains
+File("rectangle_facet_region.xml") << boundaries
+    
