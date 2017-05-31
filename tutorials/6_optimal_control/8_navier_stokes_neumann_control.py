601d88e2c663e88e528ddf757a8b2dd2b95e6559
Francesco Ballarin <francesco.ballarin@sissa.it>
2017-05-31 13:46:06 +0200

    Update tutorial 6, example 8. Most relevant changes: * correct ordering of test functions * show usage of block_restrict for unconstrained state problem * show new syntax for definition of subdomain/boundary restricted variables read from file

--- tutorials/6_optimal_control/8_navier_stokes_neumann_control.py
+++ tutorials/6_optimal_control/8_navier_stokes_neumann_control.py
@@ -1,29 +1,29 @@
-# Copyright (C) 2016-2017 by the block_ext authors
+# Copyright (C) 2016-2017 by the multiphenics authors
 #
-# This file is part of block_ext.
+# This file is part of multiphenics.
 #
-# block_ext is free software: you can redistribute it and/or modify
+# multiphenics is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# block_ext is distributed in the hope that it will be useful,
+# multiphenics is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with block_ext. If not, see <http://www.gnu.org/licenses/>.
+# along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
 #
 
 from dolfin import *
-from block_ext import *
+from multiphenics import *
 from sympy import ccode, cos, symbols
 
 """
 In this tutorial we solve the optimal control problem
 
-min J(y, u) = 1/2 \int_{\Omega} (v - v_d)^2 dx + \alpha/2 \int_{\Gamma_2} u^2 dx
+min J(y, u) = 1/2 \int_{\Omega} |v - v_d|^2 dx + \alpha/2 \int_{\Gamma_2} |u|^2 ds
 s.t.
     - \nu \Delta v + v \cdot \nabla v + \nabla p = f       in \Omega
                                            div v = 0       in \Omega
@@ -34,7 +34,7 @@ s.t.
              
 where
     \Omega                      unit square
-    u \in [L^2(\Omega)]^2       control variable
+    u \in [L^2(\Gamma_2)]^2     control variable
     v \in [H^1_0(\Omega)]^2     state velocity variable
     p \in L^2(\Omega)           state pressure variable
     \alpha > 0                  penalization parameter
@@ -46,23 +46,19 @@ using an adjoint formulation solved by a one shot approach
 """
 
 ## MESH ##
-# Interior mesh
+# Mesh
 mesh = Mesh("data/square.xml")
 boundaries = MeshFunction("size_t", mesh, "data/square_facet_region.xml")
-# Neumann boundary mesh
-boundary_mesh = Mesh("data/boundary_square_2.xml")
+# Neumann boundary
+left = MeshRestriction(mesh, "data/square_restriction_boundary_2.rtc")
 
 ## FUNCTION SPACES ##
-# Interior spaces
 Y_velocity = VectorFunctionSpace(mesh, "Lagrange", 2)
 Y_pressure = FunctionSpace(mesh, "Lagrange", 1)
 U = VectorFunctionSpace(mesh, "Lagrange", 2)
-Q_velocity = VectorFunctionSpace(mesh, "Lagrange", 2)
-Q_pressure = FunctionSpace(mesh, "Lagrange", 1)
-# Boundary control space
-boundary_U = VectorFunctionSpace(boundary_mesh, "Lagrange", 2)
-# Block space
-W = BlockFunctionSpace([Y_velocity, Y_pressure, U, Q_velocity, Q_pressure], keep=[Y_velocity, Y_pressure, boundary_U, Q_velocity, Q_pressure])
+Q_velocity = Y_velocity
+Q_pressure = Y_pressure
+W = BlockFunctionSpace([Y_velocity, Y_pressure, U, Q_velocity, Q_pressure], restrict=[None, None, left, None, None])
 
 ## PROBLEM DATA ##
 alpha = Constant(1.e-5)
@@ -90,11 +86,11 @@ test = BlockTestFunction(W)
 ds = Measure("ds")(subdomain_data=boundaries)
 
 ## OPTIMALITY CONDITIONS  ##
-r =  [nu*inner(grad(z), grad(s))*dx + inner(grad(s)*v, z)*dx + inner(grad(v)*s, z)*dx - b*div(s)*dx + inner(v - v_d, s)*dx,
-      - d*div(z)*dx                                                                                                       ,
+r =  [nu*inner(grad(z), grad(w))*dx + inner(grad(w)*v, z)*dx + inner(grad(v)*w, z)*dx - b*div(w)*dx + inner(v - v_d, w)*dx,
+      - q*div(z)*dx                                                                                                       ,
       alpha*inner(u, r)*ds(2) - inner(z, r)*ds(2)                                                                         ,
-      nu*inner(grad(v), grad(w))*dx + inner(grad(v)*v, w)*dx - p*div(w)*dx - inner(f, w)*dx - inner(u, w)*ds(2)           ,
-      - q*div(v)*dx                                                                                                        ]
+      nu*inner(grad(v), grad(s))*dx + inner(grad(v)*v, s)*dx - p*div(s)*dx - inner(f, s)*dx - inner(u, s)*ds(2)           ,
+      - d*div(v)*dx                                                                                                        ]
 dr = block_derivative(r, solution, trial)
 bc = BlockDirichletBC([[DirichletBC(W.sub(0), Constant((0., 0.)), boundaries, idx) for idx in (1, 3, 4)],
                        [],
@@ -107,13 +103,12 @@ bc = BlockDirichletBC([[DirichletBC(W.sub(0), Constant((0., 0.)), boundaries, id
 J = 0.5*inner(v - v_d, v - v_d)*dx + 0.5*alpha*inner(u, u)*ds(2)
 
 ## UNCONTROLLED FUNCTIONAL VALUE ##
-r_state = [r[3],
-           r[4]]
-dr_state = [[dr[3, 0], dr[3, 1]],
-            [dr[4, 0], dr[4, 1]]]
-bc_state = BlockDirichletBC([bc[3],
-                             bc[4]])
-solution_state = BlockFunction([v, p])
+W_state_trial = W.extract_block_sub_space((0, 1))
+W_state_test  = W.extract_block_sub_space((3, 4))
+r_state = block_restrict(r, W_state_test)
+dr_state = block_restrict(dr, [W_state_test, W_state_trial])
+bc_state = block_restrict(bc, W_state_trial)
+solution_state = block_restrict(solution, W_state_trial)
 problem_state = BlockNonlinearProblem(r_state, solution_state, bc_state, dr_state)
 solver_state = BlockPETScSNESSolver(problem_state)
 solver_state.parameters.update(snes_solver_parameters["snes_solver"])
