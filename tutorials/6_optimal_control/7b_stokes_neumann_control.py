cf9879b6f96d79000a0049ea505016ee0ed38e36
Francesco Ballarin <francesco.ballarin@sissa.it>
2017-05-31 13:58:40 +0200

    Add new example 7b to tutorial 6, on Stokes optimal control with Neumann boundary control. Contributed by Zakia Zainib <zzainib@sissa.it>

---
+++ tutorials/6_optimal_control/7b_stokes_neumann_control.py
@@ -0,0 +1,145 @@
+# Copyright (C) 2016-2017 by the multiphenics authors
+#
+# This file is part of multiphenics.
+#
+# multiphenics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# multiphenics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from dolfin import *
+from multiphenics import *
+from sympy import ccode, cos, symbols
+
+"""
+In this tutorial we solve the optimal control problem
+
+min J(y, u) = 1/2 \int_{\Omega} (v - v_d)^2 dx + \alpha1/2 \int_{\Gamma_C [id 4]} |\partial_t u|^2 ds + \alpha2/2 \int_{\Gamma_C [id 4]} |u|^2 ds
+s.t.
+- \nu \Delta v + \nabla p = f       in \Omega
+                    div v = 0       in \Omega
+                        v = g       on \Gamma_{in} [id 1]
+                        v = 0       on \Gamma_{w}  [id 2]
+   p n - \nu \partial_n v = u       on \Gamma_{N}  [id 3]
+       
+             
+where
+    \Omega                      unit square
+    u \in [L^2(Gamma_C)]^2      control variable
+    v \in [H^1_0(\Omega)]^2     state velocity variable
+    p \in L^2(\Omega)           state pressure variable
+    \alpha1, \alpha2 > 0        penalization parameters
+    v_d                         desired state
+    f                           forcing term
+    g                           inlet profile
+    
+using an adjoint formulation solved by a one shot approach
+"""
+
+## MESH ##
+# Mesh
+mesh = Mesh("data/bifurcation.xml")
+subdomains = MeshFunction("size_t", mesh, "data/bifurcation_physical_region.xml")
+boundaries = MeshFunction("size_t", mesh, "data/bifurcation_facet_region.xml")
+# Neumann control boundary
+control_boundary = MeshRestriction(mesh, "data/bifurcation_restriction_control.rtc")
+# Normal and tangent
+n = FacetNormal(mesh)
+t = as_vector([n[1], -n[0]])
+
+## FUNCTION SPACES ##
+Y_velocity = VectorElement("Lagrange", mesh.ufl_cell(), 2)
+Y_pressure = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+U = VectorElement("Lagrange", mesh.ufl_cell(), 2)
+Q_velocity = Y_velocity
+Q_pressure = Y_pressure
+W_el = BlockElement(Y_velocity, Y_pressure, U, Q_velocity, Q_pressure)
+W = BlockFunctionSpace(mesh, W_el, restrict=[None, None, control_boundary, None, None])
+
+## PROBLEM DATA ##
+nu = Constant(0.04)
+alpha_1 = Constant(0.001)
+alpha_2 = 0.1*alpha_1
+g = Expression(("10.0*a*(x[1] + 1.0)*(1.0 - x[1])", "0.0"), a=1.0, element=W.sub(0).ufl_element())
+v_d = Expression(("a*(b*10.0*(pow(x[1], 3) - pow(x[1], 2) - x[1] + 1.0)) + ((1.0-b)*10.0*(-pow(x[1], 3) - pow(x[1], 2) + x[1] + 1.0))", "0.0"), a=1.0, b=0.8, element=W.sub(0).ufl_element())
+f = Constant((0., 0.))
+
+## TRIAL/TEST FUNCTIONS ##
+trial = BlockTrialFunction(W)
+(v, p, u, z, b) = block_split(trial)
+test = BlockTestFunction(W)
+(w, q, r, s, d) = block_split(test)
+
+## MEASURES ##
+dx = Measure("dx")(subdomain_data=subdomains)
+ds = Measure("ds")(subdomain_data=boundaries)
+dS = Measure("dS")(subdomain_data=boundaries)
+
+## OPTIMALITY CONDITIONS ##
+def tracking(v, w):
+    return inner(v, w)('-')
+def penalty(u, r):
+    return alpha_1*inner(grad(u)*t, grad(r)*t) + alpha_2*inner(u, r)
+a = [[tracking(v, w)*dS(4)         , 0            , 0                  , nu*inner(grad(z), grad(w))*dx, - b*div(w)*dx], 
+     [0                            , 0            , 0                  , - q*div(z)*dx                , 0            ],
+     [0                            , 0            , penalty(u, r)*ds(3), - inner(z, r)*ds(3)          , 0            ],
+     [nu*inner(grad(v), grad(s))*dx, - p*div(s)*dx, - inner(u, s)*ds(3), 0                            , 0            ],
+     [- d*div(v)*dx                , 0            , 0                  , 0                            , 0            ]]
+f =  [0,
+      0               ,
+      0               ,
+      inner(f, s)*dx  ,
+      0                ]
+bc = BlockDirichletBC([[DirichletBC(W.sub(0), g, boundaries, 1), DirichletBC(W.sub(0), Constant((0.0, 0.0)), boundaries, 2)],
+                       [],
+                       [],
+                       [DirichletBC(W.sub(3), Constant((0.0, 0.0)), boundaries, idx) for idx in (1, 2)],
+                       []])
+
+## SOLUTION ##
+solution = BlockFunction(W)
+(v, p, u, z, b) = block_split(solution)
+
+## FUNCTIONAL ##
+J = 0.5*tracking(v - v_d, v - v_d)*dS(4) + 0.5*penalty(u, u)*ds(3)
+
+## UNCONTROLLED FUNCTIONAL VALUE ##
+W_state_trial = W.extract_block_sub_space((0, 1))
+W_state_test  = W.extract_block_sub_space((3, 4))
+a_state = block_restrict(a, [W_state_test, W_state_trial])
+A_state = block_assemble(a_state)
+f_state = block_restrict(f, W_state_test)
+F_state = block_assemble(f_state)
+bc_state = block_restrict(bc, W_state_trial)
+bc_state.apply(A_state)
+bc_state.apply(F_state)
+solution_state = block_restrict(solution, W_state_trial)
+block_solve(A_state, solution_state.block_vector(), F_state)
+print "Uncontrolled J =", assemble(J)
+plot(v, title="uncontrolled state velocity")
+plot(p, title="uncontrolled state pressure")
+interactive()
+
+## OPTIMAL CONTROL ##
+A = block_assemble(a, keep_diagonal=True)
+F = block_assemble(f)
+bc.apply(A)
+bc.apply(F)
+block_solve(A, solution.block_vector(), F)
+print "Optimal J =", assemble(J)
+plot(v, title="state velocity")
+plot(p, title="state pressure")
+plot(u, title="control")
+plot(z, title="adjoint velocity")
+plot(b, title="adjoint pressure")
+interactive()
+
---
+++ tutorials/6_optimal_control/data/generate_mesh_bifurcation.py
@@ -0,0 +1,131 @@
+# Copyright (C) 2016-2017 by the multiphenics authors
+#
+# This file is part of multiphenics.
+#
+# multiphenics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# multiphenics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from dolfin import *
+from mshr import *
+from multiphenics import *
+
+"""
+This file generates the mesh which is used in the following examples:
+    7b_stokes_neumann_control
+The test case is from section 5.5 of
+F. Negri. Reduced basis method for parametrized optimal control problems governed by PDEs. Master thesis, Politecnico di Milano, 2010-2011.
+"""
+
+# Geometrical parameters
+mu1 = 1.0
+mu2 = pi/5.0
+mu3 = pi/6.0
+mu4 = 1.0
+mu5 = 1.7
+mu6 = 2.2
+# ... and related quantities
+Y = 1.0
+X = -Y
+L = 3.0
+B = Y - mu1
+H_1 = B + (tan(mu2))*(L+mu5-L)
+H_2 = B - (tan(mu3))*(L+mu6-L)
+L_1 = mu1*cos(mu2)*sin(mu2)
+L_2 = mu1*cos(mu3)*sin(mu3)
+N = mu1*cos(mu2)*cos(mu2)
+M = - mu1*cos(mu3)*cos(mu3)
+
+
+# Create mesh
+rectangle_1 = Rectangle(Point(0.0, X), Point(L - mu4, Y))
+rectangle_2 = Rectangle(Point(L - mu4, X), Point(L, Y))
+bifurcation_1 = Polygon([Point(L, B), Point(L + mu5, H_1), Point(L + mu5 - L_1, H_1 + N), Point(L, Y)])
+bifurcation_2 = Polygon([Point(L, X), Point(L + mu6 - L_2, H_2 + M), Point(L + mu6, H_2), Point(L, B)])
+domain = rectangle_1 + rectangle_2 + bifurcation_1 + bifurcation_2
+domain.set_subdomain(1, rectangle_1)
+domain.set_subdomain(2, rectangle_2)
+domain.set_subdomain(3, bifurcation_1)
+domain.set_subdomain(4, bifurcation_2)
+mesh = generate_mesh(domain, 80)
+plot(mesh)
+interactive()
+
+# Create subdomains
+subdomains = MeshFunction("size_t", mesh, mesh.topology().dim(), mesh.domains())
+plot(subdomains)
+interactive()
+
+# Create boundaries
+class Inlet(SubDomain):
+    def inside(self, x, on_boundary):
+        return on_boundary and x[0] < DOLFIN_EPS
+
+class Boundary_D(SubDomain):
+    def inside(self, x, on_boundary):
+        return (
+            on_boundary 
+                and 
+            (
+                (x[1] > X - DOLFIN_EPS and x[1] < X+DOLFIN_EPS and x[0] >= 0.0 and x[0] <= L) 
+                    or 
+                (x[0] >= 0.0 and x[0] <= L and x[1] > Y - DOLFIN_EPS and x[1] < Y + DOLFIN_EPS) 
+                    or 
+                (x[0] >= L and x[0] <= L+mu5 and x[1] >= B and x[1] <= H_1) 
+                    or 
+                (x[0] > L and x[0] <= L+mu5-L_1  and x[1] >= Y and x[1] <= H_1+N) 
+                    or 
+                (x[0] > L and x[0] <= L+mu6 and x[1] >= H_2 and x[1] <= B) 
+                    or 
+                (x[0] > L and x[0] <= L+mu6-L_2 and x[1] >= H_2 + M and x[1] <= B)
+            )
+        )
+
+class Boundary_C(SubDomain):
+    def inside(self, x, on_boundary):
+        return (
+            on_boundary 
+                and 
+            (
+                (x[0] <= L+mu5 and x [0] >= L+mu5-L_1 and x[1] >= H_1 and x[1] <= H_1+N) 
+                    or 
+                (x[0] >= L+mu6-L_2 and x[0] <= L+mu6 and x[1] >= H_2+M and x[1] <= H_2)
+            )
+        )
+        
+class Boundary_Obs(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] < (L-mu4 +DOLFIN_EPS) and x[0] > (L-mu4 - DOLFIN_EPS) and x[1] >= X and x[1] <= Y
+        
+boundaries = FacetFunction("size_t", mesh)
+inlet = Inlet()
+inlet.mark(boundaries, 1)
+boundary_d = Boundary_D()
+boundary_d.mark(boundaries, 2)
+boundary_c = Boundary_C()
+boundary_c.mark(boundaries, 3)
+boundary_obs = Boundary_Obs()
+boundary_obs.mark(boundaries, 4)
+plot(boundaries)
+interactive()
+
+# Create restrictions
+control_restriction = MeshRestriction(mesh, boundary_c)
+
+# Save
+File("bifurcation.xml") << mesh
+File("bifurcation.pvd") << mesh
+File("bifurcation_physical_region.xml") << subdomains
+File("bifurcation_facet_region.xml") << boundaries
+File("bifurcation_restriction_control.rtc") << control_restriction
+
