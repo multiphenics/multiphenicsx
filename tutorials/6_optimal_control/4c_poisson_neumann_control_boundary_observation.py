adb35f794837587a928f690b9beebf4ed13f415d
Francesco Ballarin <francesco.ballarin@sissa.it>
2017-05-31 13:41:48 +0200

    Update tutorial 6, example 4c. Most relevant changes: * correct ordering of test functions * show new syntax for definition of subdomain/boundary restricted variables read from file

--- tutorials/6_optimal_control/4c_poisson_neumann_control_boundary_observation.py
+++ tutorials/6_optimal_control/4c_poisson_neumann_control_boundary_observation.py
@@ -1,28 +1,28 @@
-# Copyright (C) 2016-2017 by the block_ext authors
+# Copyright (C) 2016-2017 by the multiphenics authors
 #
-# This file is part of block_ext.
+# This file is part of multiphenics.
 #
-# block_ext is free software: you can redistribute it and/or modify
+# multiphenics is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# block_ext is distributed in the hope that it will be useful,
+# multiphenics is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with block_ext. If not, see <http://www.gnu.org/licenses/>.
+# along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
 #
 
 from dolfin import *
-from block_ext import *
+from multiphenics import *
 
 """
 In this tutorial we solve the optimal control problem
 
-min J(y, u) = 1/2 \int_{\Gamma_2} (y - y_d)^2 dx + \alpha/2 \int_{\Gamma_2} u^2 dx
+min J(y, u) = 1/2 \int_{\Gamma_2} (y - y_d)^2 ds + \alpha/2 \int_{\Gamma_2} u^2 ds
 s.t.
       - \Delta y = f       in \Omega
     \partial_n y = 0       on \Gamma_1
@@ -39,26 +39,22 @@ where
     f                           forcing term
     
 using an adjoint formulation solved by a one shot approach.
-Note that this is an alternative weak imposition to Lagrange multipliers
+Note that this is an alternative, to Lagrange multipliers, weak imposition 
 of y = y_d on \Gamma_2.
 """
 
 ## MESH ##
-# Interior mesh
+# Mesh
 mesh = Mesh("data/square.xml")
 boundaries = MeshFunction("size_t", mesh, "data/square_facet_region.xml")
-# Dirichlet boundary mesh
-boundary_mesh = Mesh("data/boundary_square_2.xml")
+# Dirichlet boundary
+left = MeshRestriction(mesh, "data/square_restriction_boundary_2.rtc")
 
 ## FUNCTION SPACES ##
-# Interior spaces
 Y = FunctionSpace(mesh, "Lagrange", 2)
 U = FunctionSpace(mesh, "Lagrange", 2)
-Q = FunctionSpace(mesh, "Lagrange", 2)
-# Boundary control space
-boundary_U = FunctionSpace(boundary_mesh, "Lagrange", 2)
-# Block space
-W = BlockFunctionSpace([Y, U, Q], keep=[Y, boundary_U, Q])
+Q = Y
+W = BlockFunctionSpace([Y, U, Q], restrict=[None, left, None])
 
 ## PROBLEM DATA ##
 alpha = Constant(1.e-5)
@@ -75,12 +71,12 @@ zvq = BlockTestFunction(W)
 ds = Measure("ds")(subdomain_data=boundaries)
 
 ## OPTIMALITY CONDITIONS ##
-a = [[y*q*ds(2)                , 0              , inner(grad(p), grad(q))*dx], 
-     [0                        , alpha*u*v*ds(2), - p*v*ds(2)               ],
-     [inner(grad(y),grad(z))*dx, - u*z*ds(2)    , 0                         ]]
-f =  [y_d*q*ds(2),
+a = [[y*z*ds(2)                 , 0              , inner(grad(p), grad(z))*dx], 
+     [0                         , alpha*u*v*ds(2), - p*v*ds(2)               ],
+     [inner(grad(y), grad(q))*dx, - u*q*ds(2)    , 0                         ]]
+f =  [y_d*z*ds(2),
       0          ,
-      f*z*dx      ]
+      f*q*dx      ]
 bc = BlockDirichletBC([[DirichletBC(W.sub(0), Constant(0.), boundaries, 4)],
                        [],
                        [DirichletBC(W.sub(2), Constant(0.), boundaries, 4)]])
