149b9f0cf5feb38822721faeddd2fbb1fcfcdf1d
Francesco Ballarin <francesco.ballarin@sissa.it>
2017-09-20 17:44:19 +0200

    Add tutorial on domain decomposition

---
+++ tutorials/3_lagrange_multipliers/data/generate_mesh_interface.py
@@ -0,0 +1,78 @@
+# Copyright (C) 2016-2017 by the multiphenics authors
+#
+# This file is part of multiphenics.
+#
+# multiphenics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# multiphenics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from dolfin import *
+from mshr import *
+from multiphenics import *
+
+"""
+This file generates the mesh which is used in the lagrange_multipliers_interface.py example
+"""
+
+# Create mesh
+domain = Circle(Point(0., 0.), 3.)
+domain_left = domain and Rectangle(Point(-3.0, -3.0), Point(0.0, 3.0))
+domain_right = domain and Rectangle(Point(0.0, -3.0), Point(3.0, 3.0))
+domain.set_subdomain(1, domain_left)
+domain.set_subdomain(2, domain_right)
+mesh = generate_mesh(domain, 15)
+
+# Create subdomains
+subdomains = MeshFunction("size_t", mesh, mesh.topology().dim(), mesh.domains())
+
+# Create boundaries
+class OnBoundary(SubDomain):
+    def inside(self, x, on_boundary):
+        return on_boundary
+class OnInterface(SubDomain):
+    def inside(self, x, on_boundary):
+        return near(x[0], 0.)
+
+boundaries = FacetFunction("size_t", mesh)
+on_boundary = OnBoundary()
+on_boundary.mark(boundaries, 1)
+on_interface = OnInterface()
+on_interface.mark(boundaries, 2)
+
+# Create restrictions
+class Left(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] <= 0.
+class Right(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] >= 0.
+        
+interface_restriction = MeshRestriction(mesh, on_interface)
+left = Left()
+left_restriction = MeshRestriction(mesh, left)
+right = Right()
+right_restriction = MeshRestriction(mesh, right)
+
+# Save
+File("circle.xml") << mesh
+File("circle_physical_region.xml") << subdomains
+File("circle_facet_region.xml") << boundaries
+File("circle_restriction_interface.rtc.xml") << interface_restriction
+File("circle_restriction_left.rtc.xml") << left_restriction
+File("circle_restriction_right.rtc.xml") << right_restriction
+XDMFFile("circle.xdmf").write(mesh)
+XDMFFile("circle_physical_region.xdmf").write(subdomains)
+XDMFFile("circle_facet_region.xdmf").write(boundaries)
+XDMFFile("circle_restriction_interface.rtc.xdmf").write(interface_restriction)
+XDMFFile("circle_restriction_left.rtc.xdmf").write(left_restriction)
+XDMFFile("circle_restriction_right.rtc.xdmf").write(right_restriction)
---
+++ tutorials/3_lagrange_multipliers/lagrange_multipliers_interface.py
@@ -0,0 +1,121 @@
+# Copyright (C) 2016-2017 by the multiphenics authors
+#
+# This file is part of multiphenics.
+#
+# multiphenics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# multiphenics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from dolfin import *
+import matplotlib.pyplot as plt
+from multiphenics import *
+
+"""
+In this example we solve
+    - \Delta u = f      in \Omega
+             u = 0      in \partial \Omega
+using a domain decomposition approach for \Omega = \Omega_1 \cup \Omega_2,
+and introducing a lagrange multiplier to handle the continuity of the solution across
+the interface \Gamma between \Omega_1 and \Omega_2.
+
+The resulting weak formulation is:
+    find u_1 \in V(\Omega_1), u_2 \in V(\Omega_2), \eta \in E(\Gamma)
+s.t.
+    \int_{\Omega_1} grad(u_1) \cdot grad(v_1) \dx_1 +
+    \int_{\Omega_2} grad(u_2) \cdot grad(v_2) \dx_2 +
+    \int_{\Gamma} \lambda (v_1 - v_2) \ds = 0, 
+        \forall v_1 \in V(\Omega_1), v_2 \in V(\Omega_2)
+and
+    \int_{\Gamma} \eta  (u_1 - u_2) \ds = 0,
+        \forall \eta \in E(\Gamma)
+where boundary conditions on \partial\Omega are embedded in V(.)
+"""
+
+## MESHES ##
+# Mesh
+mesh = Mesh("data/circle.xml")
+subdomains = MeshFunction("size_t", mesh, "data/circle_physical_region.xml")
+boundaries = MeshFunction("size_t", mesh, "data/circle_facet_region.xml")
+# Dirichlet boundary
+left = MeshRestriction(mesh, "data/circle_restriction_left.rtc.xml")
+right = MeshRestriction(mesh, "data/circle_restriction_right.rtc.xml")
+interface = MeshRestriction(mesh, "data/circle_restriction_interface.rtc.xml")
+
+## FUNCTION SPACES ##
+# Function space
+V = FunctionSpace(mesh, "Lagrange", 2)
+# Block function space
+W = BlockFunctionSpace([V, V, V], restrict=[left, right, interface])
+
+## TRIAL/TEST FUNCTIONS ##
+u1u2l = BlockTrialFunction(W)
+(u1, u2, l) = block_split(u1u2l)
+v1v2m = BlockTestFunction(W)
+(v1, v2, m) = block_split(v1v2m)
+
+## MEASURES ##
+dx = Measure("dx")(subdomain_data=subdomains)
+ds = Measure("ds")(subdomain_data=boundaries)
+dS = Measure("dS")(subdomain_data=boundaries)
+dS = dS(2) # restrict to the interface, which has facet ID equal to 2
+
+## ASSEMBLE ##
+a = [[inner(grad(u1), grad(v1))*dx(1), 0                              ,   l("-")*v1("-")*dS ],
+     [0                              , inner(grad(u2), grad(v2))*dx(2), - l("+")*v2("+")*dS ],
+     [m("-")*u1("-")*dS              , - m("+")*u2("+")*dS            , 0                   ]]
+f =  [v1*dx(1)                       , v2*dx(2)                       , 0                   ]
+
+bc1 = DirichletBC(W.sub(0), Constant(0.), boundaries, 1)
+bc2 = DirichletBC(W.sub(1), Constant(0.), boundaries, 1)
+bcs = BlockDirichletBC([bc1, 
+                        bc2,
+                        None])
+
+## SOLVE ##
+A = block_assemble(a)
+F = block_assemble(f)
+bcs.apply(A)
+bcs.apply(F)
+block_matlab_export(A, "A")
+block_matlab_export(F, "F")
+
+U = BlockFunction(W)
+block_solve(A, U.block_vector(), F)
+
+#plt.figure(); plot(U[0])
+#plt.figure(); plot(U[1])
+#plt.figure(); plot(U[2])
+#plt.show()
+
+## ERROR ##
+u = TrialFunction(V)
+v = TestFunction(V)
+A_ex = assemble(inner(grad(u), grad(v))*dx)
+F_ex = assemble(v*dx)
+bc_ex = DirichletBC(V, Constant(0.), boundaries, 1)
+bc_ex.apply(A_ex)
+bc_ex.apply(F_ex)
+U_ex = Function(V)
+solve(A_ex, U_ex.vector(), F_ex)
+#plt.figure(); plot(U_ex)
+#plt.show()
+err1 = Function(V)
+err1.vector().add_local(+ U_ex.vector().array())
+err1.vector().add_local(- U[0].vector().array())
+err1.vector().apply("")
+err2 = Function(V)
+err2.vector().add_local(+ U_ex.vector().array())
+err2.vector().add_local(- U[1].vector().array())
+err2.vector().apply("")
+print("Error on subdomain 1", sqrt(assemble(err1*err1*dx(1))))
+print("Error on subdomain 2", sqrt(assemble(err2*err2*dx(2))))
