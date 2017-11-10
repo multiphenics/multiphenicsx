74c80dac66772c2ae191bcbfc3c159055c163670
Francesco Ballarin <francesco.ballarin@sissa.it>
2017-11-10 11:28:49 +0100

    There should be no need to reimplement as_backend_type with pybind11

--- multiphenics/la/as_backend_type.py
+++ multiphenics/la/as_backend_type.py
@@ -16,10 +16,10 @@
 # along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from dolfin import has_pybind11
+from dolfin import as_backend_type as dolfin_as_backend_type, has_pybind11
 from multiphenics.python import cpp
 
 if has_pybind11():
-    as_backend_type = None # TODO
+    as_backend_type = dolfin_as_backend_type
 else:
     as_backend_type = cpp.as_backend_type
