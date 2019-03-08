a66d1d4893201290a8aad1b0264b3962a87efe7b
Francesco Ballarin <francesco.ballarin@sissa.it>
2019-03-08 08:42:47 +0100

    Preserve _bcs_zero_off_block_diagonal after BlockPETScMatrix algebraic operations

--- multiphenics/la/block_petsc_matrix.py
+++ multiphenics/la/block_petsc_matrix.py
@@ -16,6 +16,41 @@
 # along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
 #
 
+from numbers import Number
+from numpy import empty
 from multiphenics.python import cpp
 
 BlockPETScMatrix = cpp.la.BlockPETScMatrix
+
+# Preserve _bcs_zero_off_block_diagonal attribute for BCs application
+def preserve_bcs_zero_off_block_diagonal__first_arg_matrix_second_arg_number(operator):
+    original_operator = getattr(BlockPETScMatrix, operator)
+    def custom_operator(self, other):
+        output = original_operator(self, other)
+        if isinstance(other, Number):
+            assert hasattr(self, "_bcs_zero_off_block_diagonal")
+            output._bcs_zero_off_block_diagonal = self._bcs_zero_off_block_diagonal
+        return output
+    setattr(BlockPETScMatrix, operator, custom_operator)
+    
+for operator in ("__mul__", "__rmul__", "__imul__", "__truediv__", "__itruediv__"):
+    preserve_bcs_zero_off_block_diagonal__first_arg_matrix_second_arg_number(operator)
+    
+def preserve_bcs_zero_off_block_diagonal__first_and_second_args_matrices(operator):
+    original_operator = getattr(BlockPETScMatrix, operator)
+    def custom_operator(self, other):
+        output = original_operator(self, other)
+        assert hasattr(self, "_bcs_zero_off_block_diagonal")
+        assert hasattr(other, "_bcs_zero_off_block_diagonal")
+        assert len(self._bcs_zero_off_block_diagonal) == len(other._bcs_zero_off_block_diagonal)
+        assert all(len(self._bcs_zero_off_block_diagonal[I]) == len(other._bcs_zero_off_block_diagonal[I]) for I in range(len(self._bcs_zero_off_block_diagonal)))
+        bcs_zero_off_block_diagonal = empty((len(self._bcs_zero_off_block_diagonal), len(self._bcs_zero_off_block_diagonal[0])), dtype=bool)
+        for I in range(bcs_zero_off_block_diagonal.shape[0]):
+            for J in range(bcs_zero_off_block_diagonal.shape[1]):
+                bcs_zero_off_block_diagonal[I, J] = self._bcs_zero_off_block_diagonal[I][J] or other._bcs_zero_off_block_diagonal[I][J]
+        output._bcs_zero_off_block_diagonal = bcs_zero_off_block_diagonal.tolist()
+        return output
+    setattr(BlockPETScMatrix, operator, custom_operator)
+    
+for operator in ("__add__", "__iadd__", "__sub__", "__isub__"):
+    preserve_bcs_zero_off_block_diagonal__first_and_second_args_matrices(operator)
