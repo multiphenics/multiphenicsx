ed505dfb0abd585b99f95fb70e8b641bfcb56237
Francesco Ballarin <francesco.ballarin@sissa.it>
2019-09-26 09:58:33 +0200

    Update Stokes and Navier-Stokes distributed optimal control tutorials, with a slight regolding of cost functional asserts due to regenerated meshes. The use of a lagrange multiplier to constrain the pressure as been replaced by a penalization term, because of removal of Real elements in DOLFINX

--- tutorials/6_optimal_control/5_stokes.py
+++ tutorials/6_optimal_control/5_stokes.py
@@ -16,12 +16,16 @@
 # along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from numpy import isclose
+from numpy import isclose, isin, where
+from ufl import *
 from dolfin import *
-from dolfin import function
+from dolfin.cpp.mesh import GhostMode
+from dolfin.fem import assemble_scalar
+from dolfin.io import XDMFFile
+from dolfin.plotting import plot
 import matplotlib.pyplot as plt
 from multiphenics import *
-from sympy import ccode, cos, symbols
+from sympy import cos, lambdify, symbols
 
 r"""
 In this tutorial we solve the optimal control problem
@@ -45,78 +49,79 @@ using an adjoint formulation solved by a one shot approach
 """
 
 # MESH #
-mesh = Mesh("data/square.xml")
-boundaries = MeshFunction("size_t", mesh, "data/square_facet_region.xml")
+mesh = XDMFFile(MPI.comm_world, "data/square.xdmf").read_mesh(GhostMode.none)
+boundaries = XDMFFile(MPI.comm_world, "data/square_facet_region.xdmf").read_mf_size_t(mesh)
+boundaries_1234 = where(isin(boundaries.values, (1, 2, 3, 4)))[0]
 
 # FUNCTION SPACES #
 Y_velocity = VectorElement("Lagrange", mesh.ufl_cell(), 2)
 Y_pressure = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
 U = VectorElement("Lagrange", mesh.ufl_cell(), 2)
-L = FiniteElement("R", mesh.ufl_cell(), 0)
 Q_velocity = Y_velocity
 Q_pressure = Y_pressure
-W_el = BlockElement(Y_velocity, Y_pressure, U, L, Q_velocity, Q_pressure)
+W_el = BlockElement(Y_velocity, Y_pressure, U, Q_velocity, Q_pressure)
 W = BlockFunctionSpace(mesh, W_el)
 
 # PROBLEM DATA #
 alpha = 1.e-5
+epsilon = 1.e-5
 x, y = symbols("x[0], x[1]")
 psi_d = 10*(1-cos(0.8*pi*x))*(1-cos(0.8*pi*y))*(1-x)**2*(1-y)**2
-v_d = Expression((ccode(psi_d.diff(y, 1)), ccode(-psi_d.diff(x, 1))), element=W.sub(0).ufl_element())
-f = as_vector((0., 0.))
-@function.expression.numba_eval
-def zero_eval(values, x, cell):
+v_d_x = lambdify([x, y], psi_d.diff(y, 1))
+v_d_y = lambdify([x, y], -psi_d.diff(x, 1))
+def v_d_eval(values, x):
+    values[:, 0] = v_d_x(x[:, 0], x[:, 1])
+    values[:, 1] = v_d_y(x[:, 0], x[:, 1])
+v_d = interpolate(v_d_eval, W.sub(0))
+f = Constant(mesh, (0., 0.))
+def zero_eval(values, x):
     values[:, 0] = 0.0
     values[:, 1] = 0.0
-bc0 = interpolate(Expression(zero_eval, shape=(2,)), W.sub(0))
+bc0 = interpolate(zero_eval, W.sub(0))
 
 # TRIAL/TEST FUNCTIONS #
 trial = BlockTrialFunction(W)
-(v, p, u, l, z, b) = block_split(trial)
+(v, p, u, z, b) = block_split(trial)
 test = BlockTestFunction(W)
-(w, q, r, m, s, d) = block_split(test)
+(w, q, r, s, d) = block_split(test)
 
 # OPTIMALITY CONDITIONS #
-a = [[inner(v, w)*dx            , 0            , 0                   , 0     , inner(grad(z), grad(w))*dx, - b*div(w)*dx],
-     [0                         , 0            , 0                   , l*q*dx, - q*div(z)*dx             , 0            ],
-     [0                         , 0            , alpha*inner(u, r)*dx, 0     , - inner(z, r)*dx          , 0            ],
-     [0                         , p*m*dx       , 0                   , 0     , 0                         , 0            ],
-     [inner(grad(v), grad(s))*dx, - p*div(s)*dx, - inner(u, s)*dx    , 0     , 0                         , 0            ],
-     [- d*div(v)*dx             , 0            , 0                   , 0     , 0                         , 0            ]]
+a = [[inner(v, w)*dx            , 0             , 0                   , inner(grad(z), grad(w))*dx, - b*div(w)*dx ],
+     [0                         , 0             , 0                   , - q*div(z)*dx             , epsilon*b*q*dx],
+     [0                         , 0             , alpha*inner(u, r)*dx, - inner(z, r)*dx          , 0             ],
+     [inner(grad(v), grad(s))*dx, - p*div(s)*dx , - inner(u, s)*dx    , 0                         , 0             ],
+     [- d*div(v)*dx             , epsilon*p*d*dx, 0                   , 0                         , 0             ]]
 f =  [inner(v_d, w)*dx,
       0               ,
       0               ,
       0               ,
       inner(f, s)*dx  ,
       0                ]
-bc = BlockDirichletBC([[DirichletBC(W.sub(0), bc0, boundaries, idx) for idx in (1, 2, 3, 4)],
+bc = BlockDirichletBC([[DirichletBC(W.sub(0), bc0, boundaries_1234)],
                        [],
                        [],
-                       [],
-                       [DirichletBC(W.sub(4), bc0, boundaries, idx) for idx in (1, 2, 3, 4)],
+                       [DirichletBC(W.sub(3), bc0, boundaries_1234)],
                        []])
 
 # SOLUTION #
 solution = BlockFunction(W)
-(v, p, u, l, z, b) = block_split(solution)
+(v, p, u, z, b) = block_split(solution)
 
 # FUNCTIONAL #
 J = 0.5*inner(v - v_d, v - v_d)*dx + 0.5*alpha*inner(u, u)*dx
 
 # UNCONTROLLED FUNCTIONAL VALUE #
 W_state_trial = W.extract_block_sub_space((0, 1))
-W_state_test = W.extract_block_sub_space((4, 5))
+W_state_test = W.extract_block_sub_space((3, 4))
 a_state = block_restrict(a, [W_state_test, W_state_trial])
-A_state = block_assemble(a_state)
 f_state = block_restrict(f, W_state_test)
-F_state = block_assemble(f_state)
 bc_state = block_restrict(bc, W_state_trial)
-bc_state.apply(A_state)
-bc_state.apply(F_state)
 solution_state = block_restrict(solution, W_state_trial)
-block_solve(A_state, solution_state.block_vector, F_state)
-print("Uncontrolled J =", assemble(J))
-assert isclose(assemble(J), 0.1784540)
+solver_parameters = {"ksp_type": "preonly", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"}
+block_solve(a_state, solution_state, f_state, bc_state, petsc_options=solver_parameters)
+J_uncontrolled = MPI.sum(mesh.mpi_comm(), assemble_scalar(J))
+print("Uncontrolled J =", J_uncontrolled)
+assert isclose(J_uncontrolled, 0.1784542)
 plt.figure()
 plot(v, title="uncontrolled state velocity")
 plt.figure()
@@ -124,21 +129,16 @@ plot(p, title="uncontrolled state pressure")
 plt.show()
 
 # OPTIMAL CONTROL #
-A = block_assemble(a, keep_diagonal=True)
-F = block_assemble(f)
-bc.apply(A)
-bc.apply(F)
-block_solve(A, solution.block_vector, F)
-print("Optimal J =", assemble(J))
-assert isclose(assemble(J), 0.0052940)
+block_solve(a, solution, f, bc, petsc_options=solver_parameters)
+J_controlled = MPI.sum(mesh.mpi_comm(), assemble_scalar(J))
+print("Optimal J =", J_controlled)
+assert isclose(J_controlled, 0.0052940)
 plt.figure()
 plot(v, title="state velocity")
 plt.figure()
 plot(p, title="state pressure")
 plt.figure()
 plot(u, title="control")
-plt.figure()
-plot(l, title="lambda")
 plt.figure()
 plot(z, title="adjoint velocity")
 plt.figure()
--- tutorials/6_optimal_control/6_navier_stokes.py
+++ tutorials/6_optimal_control/6_navier_stokes.py
@@ -16,12 +16,16 @@
 # along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from numpy import isclose
+from numpy import isclose, isin, where
+from ufl import *
 from dolfin import *
-from dolfin import function
+from dolfin.cpp.mesh import GhostMode
+from dolfin.fem import assemble_scalar
+from dolfin.io import XDMFFile
+from dolfin.plotting import plot
 import matplotlib.pyplot as plt
 from multiphenics import *
-from sympy import ccode, cos, symbols
+from sympy import cos, lambdify, symbols
 
 r"""
 In this tutorial we solve the optimal control problem
@@ -46,60 +50,55 @@ using an adjoint formulation solved by a one shot approach
 """
 
 # MESH #
-mesh = Mesh("data/square.xml")
-boundaries = MeshFunction("size_t", mesh, "data/square_facet_region.xml")
+mesh = XDMFFile(MPI.comm_world, "data/square.xdmf").read_mesh(GhostMode.none)
+boundaries = XDMFFile(MPI.comm_world, "data/square_facet_region.xdmf").read_mf_size_t(mesh)
+boundaries_1234 = where(isin(boundaries.values, (1, 2, 3, 4)))[0]
 
 # FUNCTION SPACES #
 Y_velocity = VectorElement("Lagrange", mesh.ufl_cell(), 2)
 Y_pressure = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
 U = VectorElement("Lagrange", mesh.ufl_cell(), 2)
-L = FiniteElement("R", mesh.ufl_cell(), 0)
 Q_velocity = Y_velocity
 Q_pressure = Y_pressure
-W_el = BlockElement(Y_velocity, Y_pressure, U, L, Q_velocity, Q_pressure)
+W_el = BlockElement(Y_velocity, Y_pressure, U, Q_velocity, Q_pressure)
 W = BlockFunctionSpace(mesh, W_el)
 
 # PROBLEM DATA #
 alpha = 1.e-5
+epsilon = 1.e-5
 x, y = symbols("x[0], x[1]")
 psi_d = 10*(1-cos(0.8*pi*x))*(1-cos(0.8*pi*y))*(1-x)**2*(1-y)**2
-v_d = Expression((ccode(psi_d.diff(y, 1)), ccode(-psi_d.diff(x, 1))), element=W.sub(0).ufl_element())
+v_d_x = lambdify([x, y], psi_d.diff(y, 1))
+v_d_y = lambdify([x, y], -psi_d.diff(x, 1))
+def v_d_eval(values, x):
+    values[:, 0] = v_d_x(x[:, 0], x[:, 1])
+    values[:, 1] = v_d_y(x[:, 0], x[:, 1])
+v_d = interpolate(v_d_eval, W.sub(0))
 nu = 0.01
-f = as_vector((0., 0.))
-@function.expression.numba_eval
-def zero_eval(values, x, cell):
+f = Constant(mesh, (0., 0.))
+def zero_eval(values, x):
     values[:, 0] = 0.0
     values[:, 1] = 0.0
-bc0 = interpolate(Expression(zero_eval, shape=(2,)), W.sub(0))
-
-# NONLINEAR SOLVER PARAMETERS #
-snes_solver_parameters = {"nonlinear_solver": "snes",
-                          "snes_solver": {"linear_solver": "mumps",
-                                          "maximum_iterations": 20,
-                                          "report": True,
-                                          "line_search": "bt",
-                                          "error_on_nonconvergence": True}}
+bc0 = interpolate(zero_eval, W.sub(0))
 
 # TRIAL/TEST FUNCTIONS AND SOLUTION #
 trial = BlockTrialFunction(W)
 solution = BlockFunction(W)
-(v, p, u, l, z, b) = block_split(solution)
+(v, p, u, z, b) = block_split(solution)
 test = BlockTestFunction(W)
-(w, q, r, m, s, d) = block_split(test)
+(w, q, r, s, d) = block_split(test)
 
 # OPTIMALITY CONDITIONS  #
 r =  [nu*inner(grad(z), grad(w))*dx + inner(grad(w)*v, z)*dx + inner(grad(v)*w, z)*dx - b*div(w)*dx + inner(v - v_d, w)*dx,
-      - q*div(z)*dx + l*q*dx                                                                                              ,
+      - q*div(z)*dx + epsilon*b*q*dx                                                                                      ,
       alpha*inner(u, r)*dx - inner(z, r)*dx                                                                               ,
-      p*m*dx                                                                                                              ,
       nu*inner(grad(v), grad(s))*dx + inner(grad(v)*v, s)*dx - p*div(s)*dx - inner(u + f, s)*dx                           ,
-      - d*div(v)*dx                                                                                                        ]
+      - d*div(v)*dx + epsilon*p*d*dx                                                                                       ]
 dr = block_derivative(r, solution, trial)
-bc = BlockDirichletBC([[DirichletBC(W.sub(0), bc0, boundaries, idx) for idx in (1, 2, 3, 4)],
+bc = BlockDirichletBC([[DirichletBC(W.sub(0), bc0, boundaries_1234)],
                        [],
                        [],
-                       [],
-                       [DirichletBC(W.sub(4), bc0, boundaries, idx) for idx in (1, 2, 3, 4)],
+                       [DirichletBC(W.sub(3), bc0, boundaries_1234)],
                        []])
 
 
@@ -108,17 +107,18 @@ J = 0.5*inner(v - v_d, v - v_d)*dx + 0.5*alpha*inner(u, u)*dx
 
 # UNCONTROLLED FUNCTIONAL VALUE #
 W_state_trial = W.extract_block_sub_space((0, 1))
-W_state_test = W.extract_block_sub_space((4, 5))
+W_state_test = W.extract_block_sub_space((3, 4))
 r_state = block_restrict(r, W_state_test)
 dr_state = block_restrict(dr, [W_state_test, W_state_trial])
 bc_state = block_restrict(bc, W_state_trial)
 solution_state = block_restrict(solution, W_state_trial)
 problem_state = BlockNonlinearProblem(r_state, solution_state, bc_state, dr_state)
-solver_state = BlockPETScSNESSolver(problem_state)
-solver_state.parameters.update(snes_solver_parameters["snes_solver"])
-solver_state.solve()
-print("Uncontrolled J =", assemble(J))
-assert isclose(assemble(J), 0.1784540)
+solver_state = BlockNewtonSolver(mesh.mpi_comm())
+solver_state.max_it = 20
+solver_state.solve(problem_state, solution_state.block_vector)
+J_uncontrolled = MPI.sum(mesh.mpi_comm(), assemble_scalar(J))
+print("Uncontrolled J =", J_uncontrolled)
+assert isclose(J_uncontrolled, 0.1784542)
 plt.figure()
 plot(v, title="uncontrolled state velocity")
 plt.figure()
@@ -127,19 +127,18 @@ plt.show()
 
 # OPTIMAL CONTROL #
 problem = BlockNonlinearProblem(r, solution, bc, dr)
-solver = BlockPETScSNESSolver(problem)
-solver.parameters.update(snes_solver_parameters["snes_solver"])
-solver.solve()
-print("Optimal J =", assemble(J))
-assert isclose(assemble(J), 9.971933e-7)
+solver = BlockNewtonSolver(mesh.mpi_comm())
+solver.max_it = 20
+solver.solve(problem, solution.block_vector)
+J_controlled = MPI.sum(mesh.mpi_comm(), assemble_scalar(J))
+print("Optimal J =", J_controlled)
+assert isclose(J_controlled, 9.9523191e-7)
 plt.figure()
 plot(v, title="state velocity")
 plt.figure()
 plot(p, title="state pressure")
 plt.figure()
 plot(u, title="control")
-plt.figure()
-plot(l, title="lambda")
 plt.figure()
 plot(z, title="adjoint velocity")
 plt.figure()
