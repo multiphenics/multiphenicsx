5882898fae9381f7a0ebe5996c1577dcd0228c7b
Francesco Ballarin <francesco.ballarin@sissa.it>
2019-09-26 12:55:08 +0200

    Update tutorial 8

--- tutorials/8_modify_matrix_on_restriction/modify_matrix_on_restriction.py
+++ tutorials/8_modify_matrix_on_restriction/modify_matrix_on_restriction.py
@@ -16,9 +16,16 @@
 # along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from numpy import isclose
+import numpy
+from numpy import isclose, where
+from petsc4py import PETSc
+from ufl import *
 from dolfin import *
+from dolfin.cpp.mesh import GhostMode
+from dolfin.fem import assemble_scalar
 from multiphenics import *
+from multiphenics.fem import block_assemble
+from multiphenics.io import XDMFFile
 
 r"""
 In this example we solve a Laplace problem with non-homogeneous
@@ -45,15 +52,15 @@ matrices.
 
 # MESHES #
 # Mesh
-mesh = Mesh("data/circle.xml")
-subdomains = MeshFunction("size_t", mesh, "data/circle_physical_region.xml")
-boundaries = MeshFunction("size_t", mesh, "data/circle_facet_region.xml")
+mesh = XDMFFile(MPI.comm_world, "data/circle.xdmf").read_mesh(GhostMode.none)
+subdomains = XDMFFile(MPI.comm_world, "data/circle_physical_region.xdmf").read_mf_size_t(mesh)
+boundaries = XDMFFile(MPI.comm_world, "data/circle_facet_region.xdmf").read_mf_size_t(mesh)
 # Dirichlet boundary
-boundary_restriction = MeshRestriction(mesh, "data/circle_restriction_boundary.rtc.xml")
+boundary_restriction = XDMFFile(MPI.comm_world, "data/circle_restriction_boundary.rtc.xdmf").read_mesh_restriction(mesh)
 
 # FUNCTION SPACES #
 # Function space
-V = FunctionSpace(mesh, "Lagrange", 2)
+V = FunctionSpace(mesh, ("Lagrange", 2))
 # Block function space
 W = BlockFunctionSpace([V], restrict=[None])
 
@@ -117,58 +124,63 @@ def generate_penalty_system(W, component, restriction, penalty, g):
     (fenics_local_dofs, multiphenics_local_dofs) = get_local_dofs(W, component)
     (fenics_boundary_dofs, multiphenics_boundary_dofs) = get_local_dofs_on_restriction(W, component, restriction)
     fenics_interior_dofs = [dof for dof in fenics_local_dofs if dof not in fenics_boundary_dofs]
-    # Assemble penalty matrix in a multiphenics matrix. Note that, as the matrix is assembled my multiphenics,
+    # Assemble penalty matrix in a multiphenics matrix. Note that, as the matrix is assembled by multiphenics,
     # we will be using multiphenics_*_dofs variables, i.e. multiphenics numbering.
     p = [[u*v*dx]] # this u*v*dx form is not really used per se, just to allocated the correct sparsity pattern
     P = block_assemble(p)
-    P.zero()
+    P.zeroEntries()
     for dof in multiphenics_boundary_dofs:
-        P.mat().setValuesLocal([dof], [dof], [penalty]) # set P_{ij} = penalty * \delta_{ij}, if i on boundary
-    P.mat().assemble()
+        P.setValuesLocal([dof], [dof], [penalty]) # set P_{ij} = penalty * \delta_{ij}, if i on boundary
+    P.assemble()
     # Note that the for loop could have been replaced by
     #   P.mat().zeroRowsColumnsLocal(multiphenics_boundary_dofs, penalty)
     # We provide a manual version to show how to query petsc4py to manually change matrix entries (not necessarily on the
     # diagonal, but necessarily included in the sparsity pattern).
     #
-    # Next, assemble the penalty vector. First, project penalty*g on W[component], throwing away interior values.
-    # Note that here we are using fenics_*_dofs variables, because project() returns a FEniCS vector.
+    # Next, assemble the penalty vector. First, copy g and rescale by penalty, throwing away interior values.
+    # Note that here we are using fenics_*_dofs variables, because g is a FEniCS vector.
     q_function = BlockFunction(W)
-    project(penalty*g, W[component], function=q_function.sub(component))
-    q_array = q_function.sub(component).vector.get_local()
-    for dof in fenics_interior_dofs:
-        q_array[dof] = 0.
-    q_function.sub(component).vector.set_local(q_array)
-    q_function.sub(component).vector.apply("insert")
+    g.vector.copy(result=q_function.sub(component).vector)
+    q_function.sub(component).vector.scale(penalty)
+    q_function.sub(component).vector.setValuesLocal(fenics_interior_dofs, [0.]*len(fenics_interior_dofs))
+    q_function.sub(component).vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
     q_function.apply("from subfunctions", component)
     # Return matrix and vector
     return P, q_function.block_vector
 
 # ASSEMBLE #
-g = Expression("sin(3*x[0] + 1)*sin(3*x[1] + 1)", element=V.ufl_element())
+def g_eval(values, x):
+    values[:, 0] = numpy.sin(3*x[:, 0] + 1)*numpy.sin(3*x[:, 1] + 1)
+g = interpolate(g_eval, V)
 a = [[inner(grad(u), grad(v))*dx]]
 f =  [v*dx                      ]
-
-# SOLVE #
 A = block_assemble(a)
 F = block_assemble(f)
 (P, Q) = generate_penalty_system(W, 0, boundary_restriction, 1.e10, g)
 
+# SOLVE #
+# Store options
+options = PETSc.Options()
+solver_parameters = {"ksp_type": "preonly", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"}
+for k, v in solver_parameters.items():
+    options.setValue("multiphenics_solve_" + k, v)
+# Solve
 U = BlockFunction(W)
-block_solve(A + P, U.block_vector, F + Q)
+solver = PETSc.KSP().create(W.mesh.mpi_comm())
+solver.setOptionsPrefix("multiphenics_solve_")
+solver.setFromOptions()
+solver.setOperators(A + P)
+solver.solve(F + Q, U.block_vector)
+# Keep subfunctions up to date
+U.apply("to subfunctions")
 
 # ERROR #
-A_ex = assemble(a[0][0])
-F_ex = assemble(f[0])
-bc_ex = DirichletBC(V, g, boundaries, 1)
-bc_ex.apply(A_ex)
-bc_ex.apply(F_ex)
-U_ex = Function(V)
-solve(A_ex, U_ex.vector, F_ex)
-err = Function(V)
-err.vector.add_local(+ U_ex.vector.get_local())
-err.vector.add_local(- U[0].vector.get_local())
-err.vector.apply("")
-U_ex_norm = sqrt(assemble(inner(grad(U_ex), grad(U_ex))*dx))
-err_norm = sqrt(assemble(inner(grad(err), grad(err))*dx))
-print("Relative error is equal to", err_norm/U_ex_norm)
-assert isclose(err_norm/U_ex_norm, 0., atol=1.e-10)
+boundaries_1 = where(boundaries.values == 1)[0]
+bc_ex = DirichletBC(V, g, boundaries_1)
+u_ex = Function(V)
+solve(a[0][0] == f[0], u_ex, bc_ex, petsc_options=solver_parameters)
+u_ex.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+u_ex_norm = sqrt(MPI.sum(mesh.mpi_comm(), assemble_scalar(inner(grad(u_ex), grad(u_ex))*dx)))
+err_norm = sqrt(MPI.sum(mesh.mpi_comm(), assemble_scalar(inner(grad(u_ex - U[0]), grad(u_ex - U[0]))*dx)))
+print("Relative error is equal to", err_norm/u_ex_norm)
+assert isclose(err_norm/u_ex_norm, 0., atol=1.e-10)
