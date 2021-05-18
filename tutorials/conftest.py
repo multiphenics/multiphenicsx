44747f327ec10efebeb639358cdff114b5b70be7
Francesco Ballarin <francesco.ballarin@unicatt.it>
2021-05-18 10:09:11 +0200

    Use new multiphenicsx.fem module in tutorials

--- tutorials/conftest.py
+++ tutorials/conftest.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2016-2021 by the multiphenics authors
+# Copyright (C) 2016-2021 by the multiphenicsx authors
 #
-# This file is part of multiphenics.
+# This file is part of multiphenicsx.
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
