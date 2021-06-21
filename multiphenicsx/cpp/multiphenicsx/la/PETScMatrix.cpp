6d76aeea49952a452b156ae0cf33b94df488da60
Francesco Ballarin <francesco.ballarin@unicatt.it>
2021-06-21 09:39:05 +0200

    Include cassert header

--- multiphenicsx/cpp/multiphenicsx/la/PETScMatrix.cpp
+++ multiphenicsx/cpp/multiphenicsx/la/PETScMatrix.cpp
@@ -4,6 +4,7 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
+#include <cassert>
 #include <vector>
 #include <dolfinx/la/PETScVector.h>  // for dolfinx::la::petsc_error
 #include <multiphenicsx/la/PETScMatrix.h>
