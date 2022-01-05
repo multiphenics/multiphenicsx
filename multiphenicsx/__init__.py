95050b6ad193be553695bd8a46837ec43ffe5688
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-01-05 15:38:53 +0100

    Apply new set of flake8 rules to multiphenicsx main module, and its setup file

--- multiphenicsx/__init__.py
+++ multiphenicsx/__init__.py
@@ -3,7 +3,19 @@
 # This file is part of multiphenicsx.
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
+"""multiphenicsx main module."""
 
-import multiphenicsx.cpp  # noqa: F401
+# Simplify import of mpi4py.MPI and petsc4py.PETSc in internal modules (and slepc4py.SLEPc
+# in tutorials, if available) by importing them here once and for all. Internal modules will
+# now only need to import the main packages mpi4py and petsc4py (and slepc4py, if available).
+import mpi4py
+import mpi4py.MPI  # noqa: F401
+import petsc4py
+import petsc4py.PETSc  # noqa: F401
 
-__all__ = []
+try:
+    import slepc4py
+except ImportError:  # pragma: no cover
+    pass
+else:
+    import slepc4py.SLEPc  # noqa: F401
--- setup.py
+++ setup.py
@@ -3,42 +3,37 @@
 # This file is part of multiphenicsx.
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
+"""multiphenicsx setup script."""
 
-from setuptools import find_packages, setup
+import setuptools
 
-setup(name="multiphenicsx",
-      description="Easy prototyping of multiphysics problems on conforming meshes in FEniCSx",
-      long_description="Easy prototyping of multiphysics problems on conforming meshes in FEniCSx",
-      author="Francesco Ballarin (and contributors)",
-      author_email="francesco.ballarin@unicatt.it",
-      version="0.1.dev1",
-      license="GNU Library or Lesser General Public License (LGPL)",
-      url="http://mathlab.sissa.it/multiphenics",
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Intended Audience :: Developers",
-          "Intended Audience :: Science/Research",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
-          "Programming Language :: Python :: 3.9",
-          "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-          "Topic :: Scientific/Engineering :: Mathematics",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=[
-          "cppimport",
-          "pytest-runner"
-      ],
-      tests_require=[
-          "nbconvert",
-          "pytest",
-          "pytest-flake8",
-          "pytest-html",
-          "pytest-instafail",
-          "pytest-xdist"
-      ],
-      )
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+with open("tutorials/requirements.txt") as f:
+    install_requires += f.read().splitlines()
+
+setuptools.setup(
+    name="multiphenicsx",
+    description="Easy prototyping of multiphysics problems on conforming meshes in FEniCSx",
+    long_description="Easy prototyping of multiphysics problems on conforming meshes in FEniCSx",
+    author="Francesco Ballarin (and contributors)",
+    author_email="francesco.ballarin@unicatt.it",
+    version="0.1.dev1",
+    license="GNU Library or Lesser General Public License (LGPL)",
+    url="http://mathlab.sissa.it/multiphenics",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=install_requires
+)
