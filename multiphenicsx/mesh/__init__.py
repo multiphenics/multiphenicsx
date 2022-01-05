3213cd9692de4e2613e1b20de703eb02cf280213
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-01-05 15:36:43 +0100

    Apply new set of flake8 rules to multiphenicsx.mesh module (and flatten it)

--- multiphenicsx/mesh/__init__.py
+++ multiphenicsx/mesh/__init__.py
@@ -3,3 +3,6 @@
 # This file is part of multiphenicsx.
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
+"""multiphenicsx mesh module."""
+
+from multiphenicsx.mesh.gmsh_to_fenicsx import gmsh_to_fenicsx
