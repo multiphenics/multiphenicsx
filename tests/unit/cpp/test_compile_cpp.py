c64110ac76c40dc603da55de5c4555299f7638ac
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-02-03 09:37:23 +0100

    Drop dolfinx_utils in favor of nbvalx.tempfile

--- tests/unit/cpp/test_compile_cpp.py
+++ tests/unit/cpp/test_compile_cpp.py
@@ -8,114 +8,115 @@
 import os
 
 import dolfinx.jit
-import dolfinx_utils.test.fixtures
 import mpi4py
+import nbvalx.tempfile
 
 import multiphenicsx.cpp
 import multiphenicsx.io
 
-tempdir = dolfinx_utils.test.fixtures.tempdir
 
-
-def test_compile_code(tempdir: str) -> None:
+def test_compile_code() -> None:
     """Compile a simple C++ function."""
     comm = mpi4py.MPI.COMM_WORLD
 
-    if comm.rank == 0:
-        code = """
-#include <pybind11/pybind11.h>
+    with nbvalx.tempfile.TemporaryDirectory(comm) as tempdir:
+        if comm.rank == 0:
+            code = """
+    #include <pybind11/pybind11.h>
 
-int multiply(int a, int b) {
-    return a * b;
-}
+    int multiply(int a, int b) {
+        return a * b;
+    }
 
-PYBIND11_MODULE(SIGNATURE, m)
-{
-    m.def("multiply", &multiply);
-}
-"""
-        filename = os.path.join(tempdir, "test_compile_code_source.cpp")
-        open(filename, "w").write(code)
-        filename = comm.bcast(filename, root=0)
-    else:
-        filename = comm.bcast(None, root=0)
+    PYBIND11_MODULE(SIGNATURE, m)
+    {
+        m.def("multiply", &multiply);
+    }
+    """
+            filename = os.path.join(tempdir, "test_compile_code_source.cpp")
+            open(filename, "w").write(code)
+            filename = comm.bcast(filename, root=0)
+        else:
+            filename = comm.bcast(None, root=0)
 
-    compile_code = dolfinx.jit.mpi_jit_decorator(multiphenicsx.cpp.compile_code)
-    cpp_library = compile_code(comm, "test_compile_code", filename, output_dir=tempdir)
-    assert cpp_library.multiply(2, 3) == 6
+        compile_code = dolfinx.jit.mpi_jit_decorator(multiphenicsx.cpp.compile_code)
+        print(comm.rank, os.path.exists(tempdir), os.path.exists(os.path.join(tempdir, "test_compile_code_source.cpp")))
+        cpp_library = compile_code(comm, "test_compile_code", filename, output_dir=tempdir)
+        assert cpp_library.multiply(2, 3) == 6
 
 
-def test_compile_package(tempdir: str) -> None:
+def test_compile_package() -> None:
     """Compile a simple C++ package."""
     comm = mpi4py.MPI.COMM_WORLD
 
-    if comm.rank == 0:
-        package_root = os.path.join(tempdir, "test_compile_package")
-        os.makedirs(os.path.join(package_root, "utilities"))
-        os.makedirs(os.path.join(package_root, "wrappers"))
+    with nbvalx.tempfile.TemporaryDirectory(comm) as tempdir:
+        if comm.rank == 0:
+            package_root = os.path.join(tempdir, "test_compile_package")
+            os.makedirs(os.path.join(package_root, "utilities"))
+            os.makedirs(os.path.join(package_root, "wrappers"))
 
-        multiply_header_code = """
-namespace utilities
-{
-    int multiply(int a, int b);
-}
-"""
-        multiply_header_file = os.path.join("utilities", "multiply.h")
-        open(os.path.join(package_root, multiply_header_file), "w").write(multiply_header_code)
-
-        multiply_source_code = """
-#include <test_compile_package/utilities/multiply.h>
-
-int utilities::multiply(int a, int b)
-{
-    return a * b;
-}
-"""
-        multiply_source_file = os.path.join("utilities", "multiply.cpp")
-        open(os.path.join(package_root, multiply_source_file), "w").write(multiply_source_code)
-
-        utilities_wrapper_code = """
-#include <pybind11/pybind11.h>
-
-#include <test_compile_package/utilities/multiply.h>
-
-namespace py = pybind11;
-
-namespace wrappers
-{
-    void utilities(py::module& m)
+            multiply_header_code = """
+    namespace utilities
     {
-        m.def("multiply", &utilities::multiply);
+        int multiply(int a, int b);
     }
-}
-"""
-        utilities_wrapper_file = os.path.join("wrappers", "utilities.cpp")
-        open(os.path.join(package_root, utilities_wrapper_file), "w").write(utilities_wrapper_code)
+    """
+            multiply_header_file = os.path.join("utilities", "multiply.h")
+            open(os.path.join(package_root, multiply_header_file), "w").write(multiply_header_code)
 
-        main_wrapper_code = """
-#include <pybind11/pybind11.h>
+            multiply_source_code = """
+    #include <test_compile_package/utilities/multiply.h>
 
-namespace py = pybind11;
+    int utilities::multiply(int a, int b)
+    {
+        return a * b;
+    }
+    """
+            multiply_source_file = os.path.join("utilities", "multiply.cpp")
+            open(os.path.join(package_root, multiply_source_file), "w").write(multiply_source_code)
 
-namespace wrappers
-{
-    void utilities(py::module& m);
-}
+            utilities_wrapper_code = """
+    #include <pybind11/pybind11.h>
 
-PYBIND11_MODULE(SIGNATURE, m)
-{
-    py::module utilities = m.def_submodule("utilities", "utilities module");
-    wrappers::utilities(utilities);
-}
-"""
-        main_wrapper_file = os.path.join("wrappers", "test_compile_package.cpp")
-        open(os.path.join(package_root, main_wrapper_file), "w").write(main_wrapper_code)
+    #include <test_compile_package/utilities/multiply.h>
 
-        multiply_source_file = comm.bcast(multiply_source_file, root=0)
-    else:
-        multiply_source_file = comm.bcast(None, root=0)
-    sources = [multiply_source_file]
+    namespace py = pybind11;
 
-    cpp_library = multiphenicsx.cpp.compile_package(
-        comm, "test_compile_package", tempdir, *sources, output_dir=tempdir)
-    assert cpp_library.utilities.multiply(2, 3) == 6
+    namespace wrappers
+    {
+        void utilities(py::module& m)
+        {
+            m.def("multiply", &utilities::multiply);
+        }
+    }
+    """
+            utilities_wrapper_file = os.path.join("wrappers", "utilities.cpp")
+            open(os.path.join(package_root, utilities_wrapper_file), "w").write(utilities_wrapper_code)
+
+            main_wrapper_code = """
+    #include <pybind11/pybind11.h>
+
+    namespace py = pybind11;
+
+    namespace wrappers
+    {
+        void utilities(py::module& m);
+    }
+
+    PYBIND11_MODULE(SIGNATURE, m)
+    {
+        py::module utilities = m.def_submodule("utilities", "utilities module");
+        wrappers::utilities(utilities);
+    }
+    """
+            main_wrapper_file = os.path.join("wrappers", "test_compile_package.cpp")
+            open(os.path.join(package_root, main_wrapper_file), "w").write(main_wrapper_code)
+
+            multiply_source_file = comm.bcast(multiply_source_file, root=0)
+        else:
+            multiply_source_file = comm.bcast(None, root=0)
+        sources = [multiply_source_file]
+
+        cpp_library = multiphenicsx.cpp.compile_package(
+            comm, "test_compile_package", tempdir, *sources, output_dir=tempdir)
+        assert cpp_library.utilities.multiply(2, 3) == 6
--- tests/unit/io/test_plotting.py
+++ tests/unit/io/test_plotting.py
@@ -8,15 +8,13 @@
 import os
 
 import dolfinx.mesh
-import dolfinx_utils.test.fixtures
 import mpi4py
+import nbvalx.tempfile
 import numpy as np
 import petsc4py
 import pytest
 
 import multiphenicsx.io
-
-tempdir = dolfinx_utils.test.fixtures.tempdir
 
 # Use go and itkwidgets from multiphenicsx.io.plotting rather than the actual libraries
 # to handle the case of missing dependencies
@@ -38,111 +36,131 @@ def mesh_2d() -> dolfinx.mesh.Mesh:
     return dolfinx.mesh.create_unit_square(comm, 2 * comm.size, 2 * comm.size)
 
 
-def write_plotly_image(fig: go_or_mock.Figure, directory: str, filename: str) -> None:
+def write_plotly_image(comm: mpi4py.MPI.Intracomm, fig: go_or_mock.Figure, directory: str, filename: str) -> None:
     """Write a plotly figure to file."""
     pytest.importorskip("kaleido")
-    fig.write_image(os.path.join(directory, filename + ".png"))
+    fig.write_image(os.path.join(directory, filename + "-" + str(comm.rank) + ".png"))
 
 
-def write_itkwidgets_image(viewer: itkwidgets_or_mock.Viewer, directory: str, filename: str) -> None:
+def write_itkwidgets_image(
+    comm: mpi4py.MPI.Intracomm, viewer: itkwidgets_or_mock.Viewer, directory: str, filename: str
+) -> None:
     """Write a itkwidgets figure to file."""
     # Currently untested. itkwidgets suggests to use ipywebrtc, but it does not seem to be working
     # when called from plain python files (error on no data to export) rather than jupyter notebooks
     pass
 
 
-def test_plot_mesh_1d(mesh_1d: dolfinx.mesh.Mesh, tempdir: str) -> None:
+def test_plot_mesh_1d(mesh_1d: dolfinx.mesh.Mesh) -> None:
     """Check that plot_mesh executes without errors (1D case)."""
     pytest.importorskip("plotly")
-    write_plotly_image(multiphenicsx.io.plot_mesh(mesh_1d), tempdir, "mesh")
+    with nbvalx.tempfile.TemporaryDirectory(mesh_1d.comm) as tempdir:
+        write_plotly_image(mesh_1d.comm, multiphenicsx.io.plot_mesh(mesh_1d), tempdir, "mesh")
 
 
-def test_plot_mesh_2d(mesh_2d: dolfinx.mesh.Mesh, tempdir: str) -> None:
+def test_plot_mesh_2d(mesh_2d: dolfinx.mesh.Mesh) -> None:
     """Check that plot_mesh executes without errors (2D case)."""
     pytest.importorskip("pyvista")
-    write_itkwidgets_image(multiphenicsx.io.plot_mesh(mesh_2d), tempdir, "mesh")
+    with nbvalx.tempfile.TemporaryDirectory(mesh_2d.comm) as tempdir:
+        write_itkwidgets_image(mesh_2d.comm, multiphenicsx.io.plot_mesh(mesh_2d), tempdir, "mesh")
 
 
-def test_plot_mesh_entities_2d(mesh_2d: dolfinx.mesh.Mesh, tempdir: str) -> None:
+def test_plot_mesh_entities_2d(mesh_2d: dolfinx.mesh.Mesh) -> None:
     """Check that plot_mesh_entities executes without errors (2D mesh, 2D entities)."""
     pytest.importorskip("pyvista")
     cell_entities = dolfinx.mesh.locate_entities(
         mesh_2d, mesh_2d.topology.dim, lambda x: np.full((x.shape[1], ), True))
-    write_itkwidgets_image(
-        multiphenicsx.io.plot_mesh_entities(mesh_2d, mesh_2d.topology.dim, cell_entities),
-        tempdir, "mesh_entities")
+    with nbvalx.tempfile.TemporaryDirectory(mesh_2d.comm) as tempdir:
+        write_itkwidgets_image(
+            mesh_2d.comm, multiphenicsx.io.plot_mesh_entities(mesh_2d, mesh_2d.topology.dim, cell_entities),
+            tempdir, "mesh_entities")
 
 
-def test_plot_mesh_entities_boundary_2d(mesh_2d: dolfinx.mesh.Mesh, tempdir: str) -> None:
+def test_plot_mesh_entities_boundary_2d(mesh_2d: dolfinx.mesh.Mesh) -> None:
     """Check that plot_mesh_entities executes without errors (2D mesh, 1D entities)."""
     pytest.importorskip("pyvista")
     boundary_entities = dolfinx.mesh.locate_entities_boundary(
         mesh_2d, mesh_2d.topology.dim - 1, lambda x: np.full((x.shape[1], ), True))
-    write_itkwidgets_image(
-        multiphenicsx.io.plot_mesh_entities(mesh_2d, mesh_2d.topology.dim - 1, boundary_entities),
-        tempdir, "mesh_entities")
+    with nbvalx.tempfile.TemporaryDirectory(mesh_2d.comm) as tempdir:
+        write_itkwidgets_image(
+            mesh_2d.comm, multiphenicsx.io.plot_mesh_entities(mesh_2d, mesh_2d.topology.dim - 1, boundary_entities),
+            tempdir, "mesh_entities")
 
 
-def test_plot_mesh_tags_2d(mesh_2d: dolfinx.mesh.Mesh, tempdir: str) -> None:
+def test_plot_mesh_tags_2d(mesh_2d: dolfinx.mesh.Mesh) -> None:
     """Check that plot_mesh_tags executes without errors (2D mesh, 2D tags)."""
     pytest.importorskip("pyvista")
     cell_entities = dolfinx.mesh.locate_entities(
         mesh_2d, mesh_2d.topology.dim, lambda x: np.full((x.shape[1], ), True))
     cell_tags = dolfinx.mesh.MeshTags(
         mesh_2d, mesh_2d.topology.dim, cell_entities, np.ones_like(cell_entities))
-    write_itkwidgets_image(multiphenicsx.io.plot_mesh_tags(cell_tags), tempdir, "mesh_tags")
+    with nbvalx.tempfile.TemporaryDirectory(mesh_2d.comm) as tempdir:
+        write_itkwidgets_image(mesh_2d.comm, multiphenicsx.io.plot_mesh_tags(cell_tags), tempdir, "mesh_tags")
 
 
-def test_plot_mesh_tags_boundary_2d(mesh_2d: dolfinx.mesh.Mesh, tempdir: str) -> None:
+def test_plot_mesh_tags_boundary_2d(mesh_2d: dolfinx.mesh.Mesh) -> None:
     """Check that plot_mesh_tags executes without errors (2D mesh, 1D tags)."""
     pytest.importorskip("pyvista")
     boundary_entities = dolfinx.mesh.locate_entities_boundary(
         mesh_2d, mesh_2d.topology.dim - 1, lambda x: np.full((x.shape[1], ), True))
     boundary_tags = dolfinx.mesh.MeshTags(
         mesh_2d, mesh_2d.topology.dim - 1, boundary_entities, np.ones_like(boundary_entities))
-    write_itkwidgets_image(multiphenicsx.io.plot_mesh_tags(boundary_tags), tempdir, "mesh_tags")
+    with nbvalx.tempfile.TemporaryDirectory(mesh_2d.comm) as tempdir:
+        write_itkwidgets_image(mesh_2d.comm, multiphenicsx.io.plot_mesh_tags(boundary_tags), tempdir, "mesh_tags")
 
 
-def test_plot_scalar_field_1d(mesh_1d: dolfinx.mesh.Mesh, tempdir: str) -> None:
+def test_plot_scalar_field_1d(mesh_1d: dolfinx.mesh.Mesh) -> None:
     """Check that plot_scalar_field executes without errors (1D case)."""
     pytest.importorskip("plotly")
     V = dolfinx.fem.FunctionSpace(mesh_1d, ("Lagrange", 1))
     u = dolfinx.fem.Function(V)
     with u.vector.localForm() as u_local:
         u_local[:] = np.arange(u_local.size)
-    if not np.issubdtype(petsc4py.PETSc.ScalarType, np.complexfloating):
-        write_plotly_image(multiphenicsx.io.plot_scalar_field(u, "u"), tempdir, "u")
-    else:
-        write_plotly_image(multiphenicsx.io.plot_scalar_field(u, "u", part="real"), tempdir, "real_u")
-        write_plotly_image(multiphenicsx.io.plot_scalar_field(u, "u", part="imag"), tempdir, "imag_u")
+    with nbvalx.tempfile.TemporaryDirectory(mesh_1d.comm) as tempdir:
+        if not np.issubdtype(petsc4py.PETSc.ScalarType, np.complexfloating):
+            write_plotly_image(mesh_1d.comm, multiphenicsx.io.plot_scalar_field(u, "u"), tempdir, "u")
+        else:
+            write_plotly_image(
+                mesh_1d.comm, multiphenicsx.io.plot_scalar_field(u, "u", part="real"), tempdir, "real_u")
+            write_plotly_image(
+                mesh_1d.comm, multiphenicsx.io.plot_scalar_field(u, "u", part="imag"), tempdir, "imag_u")
 
 
-def test_plot_scalar_field_2d(mesh_2d: dolfinx.mesh.Mesh, tempdir: str) -> None:
+def test_plot_scalar_field_2d(mesh_2d: dolfinx.mesh.Mesh) -> None:
     """Check that plot_scalar_field executes without errors (2D case)."""
     pytest.importorskip("pyvista")
     V = dolfinx.fem.FunctionSpace(mesh_2d, ("Lagrange", 1))
     u = dolfinx.fem.Function(V)
     with u.vector.localForm() as u_local:
         u_local[:] = np.arange(u_local.size)
-    if not np.issubdtype(petsc4py.PETSc.ScalarType, np.complexfloating):
-        write_itkwidgets_image(multiphenicsx.io.plot_scalar_field(u, "u"), tempdir, "u")
-    else:
-        write_itkwidgets_image(multiphenicsx.io.plot_scalar_field(u, "u", part="real"), tempdir, "real_u")
-        write_itkwidgets_image(multiphenicsx.io.plot_scalar_field(u, "u", part="imag"), tempdir, "imag_u")
-    write_itkwidgets_image(multiphenicsx.io.plot_scalar_field(u, "u", warp_factor=1.0), tempdir, "glyph_u")
+    with nbvalx.tempfile.TemporaryDirectory(mesh_2d.comm) as tempdir:
+        if not np.issubdtype(petsc4py.PETSc.ScalarType, np.complexfloating):
+            write_itkwidgets_image(mesh_2d.comm, multiphenicsx.io.plot_scalar_field(u, "u"), tempdir, "u")
+        else:
+            write_itkwidgets_image(
+                mesh_2d.comm, multiphenicsx.io.plot_scalar_field(u, "u", part="real"), tempdir, "real_u")
+            write_itkwidgets_image(
+                mesh_2d.comm, multiphenicsx.io.plot_scalar_field(u, "u", part="imag"), tempdir, "imag_u")
+        write_itkwidgets_image(
+            mesh_2d.comm, multiphenicsx.io.plot_scalar_field(u, "u", warp_factor=1.0), tempdir, "glyph_u")
 
 
-def test_plot_vector_field_2d(mesh_2d: dolfinx.mesh.Mesh, tempdir: str) -> None:
+def test_plot_vector_field_2d(mesh_2d: dolfinx.mesh.Mesh) -> None:
     """Check that plot_vector_field executes without errors (2D case)."""
     pytest.importorskip("pyvista")
     V = dolfinx.fem.VectorFunctionSpace(mesh_2d, ("Lagrange", 1))
     u = dolfinx.fem.Function(V)
     with u.vector.localForm() as u_local:
         u_local[:] = np.arange(u_local.size)
-    if not np.issubdtype(petsc4py.PETSc.ScalarType, np.complexfloating):
-        write_itkwidgets_image(multiphenicsx.io.plot_vector_field(u, "u"), tempdir, "u")
-    else:
-        write_itkwidgets_image(multiphenicsx.io.plot_vector_field(u, "u", part="real"), tempdir, "real_u")
-        write_itkwidgets_image(multiphenicsx.io.plot_vector_field(u, "u", part="imag"), tempdir, "imag_u")
-    write_itkwidgets_image(multiphenicsx.io.plot_vector_field(u, "u", glyph_factor=1.0), tempdir, "glyph_u")
-    write_itkwidgets_image(multiphenicsx.io.plot_vector_field(u, "u", warp_factor=1.0), tempdir, "warp_u")
+    with nbvalx.tempfile.TemporaryDirectory(mesh_2d.comm) as tempdir:
+        if not np.issubdtype(petsc4py.PETSc.ScalarType, np.complexfloating):
+            write_itkwidgets_image(mesh_2d.comm, multiphenicsx.io.plot_vector_field(u, "u"), tempdir, "u")
+        else:
+            write_itkwidgets_image(
+                mesh_2d.comm, multiphenicsx.io.plot_vector_field(u, "u", part="real"), tempdir, "real_u")
+            write_itkwidgets_image(
+                mesh_2d.comm, multiphenicsx.io.plot_vector_field(u, "u", part="imag"), tempdir, "imag_u")
+        write_itkwidgets_image(
+            mesh_2d.comm, multiphenicsx.io.plot_vector_field(u, "u", glyph_factor=1.0), tempdir, "glyph_u")
+        write_itkwidgets_image(
+            mesh_2d.comm, multiphenicsx.io.plot_vector_field(u, "u", warp_factor=1.0), tempdir, "warp_u")
