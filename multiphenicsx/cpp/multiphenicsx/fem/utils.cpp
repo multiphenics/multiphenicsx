080007683a3702bf678ec7a4a8c21a600edb916e
Francesco Ballarin <francesco.ballarin@unicatt.it>
2023-11-07 07:09:16 +0100

    Drop empty cpp files

--- multiphenicsx/cpp/multiphenicsx/fem/utils.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2016-2023 by the multiphenicsx authors
-//
-// This file is part of multiphenicsx.
-//
-// SPDX-License-Identifier: LGPL-3.0-or-later
-
-#include <multiphenicsx/fem/utils.h>
