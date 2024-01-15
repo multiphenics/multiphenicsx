ee8cdda2ff5f5f4659946ff115e791757b1376ce
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-15 13:04:19 +0100

    Drop nbvalx hooks which have been removed upstream

--- tutorials/conftest.py
+++ tutorials/conftest.py
@@ -10,6 +10,3 @@ import nbvalx.pytest_hooks_notebooks
 pytest_addoption = nbvalx.pytest_hooks_notebooks.addoption
 pytest_sessionstart = nbvalx.pytest_hooks_notebooks.sessionstart
 pytest_collect_file = nbvalx.pytest_hooks_notebooks.collect_file
-pytest_runtest_setup = nbvalx.pytest_hooks_notebooks.runtest_setup
-pytest_runtest_makereport = nbvalx.pytest_hooks_notebooks.runtest_makereport
-pytest_runtest_teardown = nbvalx.pytest_hooks_notebooks.runtest_teardown
